72e648fc10bbe0776beb3858ea328ebac869b83c
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2023-09-12 17:06:03 -0700

    assets, metadata

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,14 @@
-<script>
+<script lang="ts">
 	import '../app.css';
+
+  const TITLE = 'Pride Frames by melanie kat';
+  const DESCRIPTION = 'Surround yourself with pride flags';
 </script>
 
+<svelte:head>
+  <title>{TITLE}</title>
+  <meta name="description" content={DESCRIPTION} />
+  <meta property="og:title" content={TITLE} />
+  <meta property="og:description" content={DESCRIPTION} />
+</svelte:head>
 <slot />
